c429864d2c58f419488efdf12fe68618834b7f54
Altuğ Bülbül <altgblbl@gmail.com>
2020-02-25 19:52:54 +0300

    Update server.js
    some comments addded

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// need imports here
 const express = require('express')
 const app = express();
 const exec = require('child_process').exec;
-// var exec = require('ssh-exec')
 
+// get respond function 
 app.get('/',(req,res)=>{
+     
+     //command type 
      const firstcommand = "ls";
     
-     
+     //execute command ssh 
     exec(firstcommand, function (err, stdout, stderr) {
         console.log(err, stdout, stderr)
       })
+     
+     // send message to website for client side
     res.send('done');
     
 });
-
+// listen port and console message for server
 app.listen(3000,console.log('server started'));
